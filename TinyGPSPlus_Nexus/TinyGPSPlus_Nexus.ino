b09620e4d2d8752e296fb4ec3923a0edac025cb1
Stacey Szigeti <staceyszigeti@gmail.com>
2018-03-17 18:11:30 +0100

    a

--- TinyGPSPlus_Nexus/TinyGPSPlus_Nexus.ino
+++ TinyGPSPlus_Nexus/TinyGPSPlus_Nexus.ino
@@ -20,35 +20,26 @@ void setup()
 {
   Serial.begin(9600);
   ss.begin(4800);
-    int tickEvent = t.every(2000, doSomething);
+  int tickEvent = t.every(2000, doSomething);
 }
 
 void loop()
 {
       t.update();
-  
+
+
+    data = String(awa.value()) + "," + String(aws.value()) + "," + String(windRef.value());
+
 
     while (ss.available() > 0)
     nexus.encode(ss.read());
-  
-/*while (ss.available() > 0)
-      Serial.write(ss.read());*/
 
+    
 }
 
 void doSomething()
 {
-  if (HIGH) {
-    data = String(awa.value()) + "," + String(aws.value()) + "," + String(windRef.value());
-  };
-  
-    /*Serial.print(F(" AWA=")); Serial.print(awa.value()); 
-    Serial.print(F(" AWS=")); Serial.print(aws.value()); 
-    Serial.print(F(" AWA1=")); Serial.print(aw1.value()); 
-    Serial.print(F(" AWS2=")); Serial.print(aw2.value()); 
-    Serial.print(F(" AWA3=")); Serial.print(aw3.value()); 
-    Serial.print(F(" AWS4=")); Serial.print(aw4.value()); 
-    Serial.print(F(" AWS5=")); Serial.println(aw5.value()); */
+
 
     Serial.println(data);
 
