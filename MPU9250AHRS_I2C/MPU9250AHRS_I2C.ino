df6f48e3ae07b49deca5ab53aa6f32e0960d20c5
Stacey Szigeti <staceyszigeti@gmail.com>
2019-01-12 16:10:25 +0100

    ahrs_cleaned

--- MPU9250AHRS_I2C/MPU9250AHRS_I2C.ino
+++ MPU9250AHRS_I2C/MPU9250AHRS_I2C.ino
@@ -28,7 +28,7 @@
 #include "MPU9250.h"
 
 
-
+/*
 #ifdef LCD
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
@@ -40,7 +40,7 @@
 // pin 5 - LCD chip select (CS)
 // pin 6 - LCD reset (RST)
 Adafruit_PCD8544 display = Adafruit_PCD8544(9, 8, 7, 5, 6);
-#endif // LCD
+#endif // LCD*/
 
 #define AHRS true         // Set to false for basic data read
 #define SerialDebug true  // Set to true to get Serial output for debugging
@@ -51,7 +51,8 @@ int myLed  = D4;  // Set up pin 13 led for toggling
 
 #define I2Cclock 400000
 #define I2Cport Wire
-#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0   // Use either this line or the next to select which I2C address your device is using
+#define MPU9250_ADDRESS 0x68
+//#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0   // Use either this line or the next to select which I2C address your device is using
 //#define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
 
 MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
@@ -353,7 +354,7 @@ void loop()
         Serial.print((float)myIMU.sumCount / myIMU.sum, 2);
         Serial.println(" Hz");*/
       }
-
+/*
 #ifdef LCD
       display.clearDisplay();
 
@@ -398,7 +399,7 @@ void loop()
       display.print((float) myIMU.sumCount / myIMU.sum, 2);
       display.print(" Hz");
       display.display();
-#endif // LCD
+#endif // LCD*/
 
       myIMU.count = millis();
       myIMU.sumCount = 0;
