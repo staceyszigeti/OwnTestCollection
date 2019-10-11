8ca6dc5e7bd79f578088daf7f6f4697e7946a433
Stacey Szigeti <staceyszigeti@gmail.com>
2019-10-11 19:42:01 +0800

    deepsleep

--- esp12f_deepsleep-test/esp12f_deepsleep-test.ino
+++ esp12f_deepsleep-test/esp12f_deepsleep-test.ino
@@ -45,7 +45,11 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 
-  // Deep sleep mode until RESET pin is connected to a LOW signal (for example pushbutton or magnetic reed switch)
+  /*// Deep sleep mode until RESET pin is connected to a LOW signal (for example pushbutton or magnetic reed switch)
   Serial.println("I'm awake, but I'm going into deep sleep mode until RESET pin is connected to a LOW signal");
-  ESP.deepSleep(0); 
+  ESP.deepSleep(0); */
+
+  // Deep sleep mode for 30 seconds, the ESP8266 wakes up by itself when GPIO 16 (D0 in NodeMCU board) is connected to the RESET pin
+  Serial.println("I'm awake, but I'm going into deep sleep mode for 30 seconds");
+  ESP.deepSleep(10e6); 
 }
