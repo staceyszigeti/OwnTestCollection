ede1738b618c9234fc744962595d4b155c2f7a30
Stacey Szigeti <staceyszigeti@gmail.com>
2019-10-12 12:12:36 +0800

    test

--- I2C_arduino-to-arduino/esp_master-send/esp_master-send.ino
+++ I2C_arduino-to-arduino/esp_master-send/esp_master-send.ino
@@ -2,6 +2,8 @@
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
 
+Adafruit_BMP280 bmp; // I2C
+
 int x = 3;
 
 void setup() {
@@ -14,12 +16,31 @@ void setup() {
   
   // put your setup code here, to run once:
   Wire.begin(10);               // join i2c bus (address optional for master)
-  Wire.beginTransmission(8); // transmit to device #8
-  Wire.write(x);              // sends one byte
-  Wire.endTransmission();    // stop transmitting
+  };
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+    /*Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! 
+    Serial.println(" m");
+
+    Serial.println();*/
+  
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(x);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    
+    delay(1000);
 
 }
